d21794e6d68e1741c2068b4a18442d9e4a881364
Aravind M <aravind29042001@gmail.com>
2024-05-31 15:57:48 +0530

    first commmit

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Aravind",
+    Age: "23",
+  },
+  {
+    id: "2",
+    Name: "Ram",
+    Age: "27",
+  },
+  {
+    id: "3",
+    Name: "Seetha",
+    Age: "25",
+  },
+];
+
+export default Employees;
